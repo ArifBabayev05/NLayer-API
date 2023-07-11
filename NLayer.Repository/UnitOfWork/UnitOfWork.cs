83d5de3b6babe59eb05e845e8e6a4afefb5d6a35
Arif Babayev <arifrb@code.edu.az>
2023-07-11 20:00:55 +0400

    Connection string creates

---
+++ NLayer.Repository/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NLayer.Core.UnitOfWorks;
+
+namespace NLayer.Repository.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        public UnitOfWork(AppDbContext context)
+        {
+            context = _context;
+        }
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
+
