ef8ed564b1e29ddeff274daca4f7357c41457aa0
Arif Babayev <arifrb@code.edu.az>
2023-07-12 16:36:41 +0400

    DTOs created

---
+++ NLayer.Core/DTOs/BaseDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace NLayer.Core.DTOs
+{
+	public abstract class BaseDTO
+	{
+		public int Id { get; set; }
+		public DateTime CreaetedDate { get; set; }
+	}
+}
+
---
+++ NLayer.Core/DTOs/CategoryDTO.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace NLayer.Core.DTOs
+{
+	public class CategoryDTO :BaseDTO
+	{
+        public string Name { get; set; }
+    }
+}
+
