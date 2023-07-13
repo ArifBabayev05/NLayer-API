ddac6a79d37b401b57311c275de5e8957b52f9c1
Arif Babayev <arifrb@code.edu.az>
2023-07-13 17:58:17 +0400

    Cleaning at controller

--- NLayer.API/Controllers/ProductController.cs
+++ NLayer.API/Controllers/ProductController.cs
@@ -11,21 +11,20 @@ namespace NLayer.API.Controllers
 	public class ProductController:CustomBaseController
 	{
 		private readonly IMapper _mapper;
-        private readonly IService<Product> _service;
-        private readonly IProductService _productService;
+        
+        private readonly IProductService _service;
 
         public ProductController(IMapper mapper, IService<Product> service, IProductService productService)
         {
             _mapper = mapper;
-            _service = service;
-            _productService = productService;
+            _service = productService;
         }
 
         //api/[controller]/GetProductsWithCategory
         [HttpGet("[action]")]
         public async Task<IActionResult> GetProductsWithCategory()
         {
-            return CreateActionResult(await _productService.GetProductsWithCategory());
+            return CreateActionResult(await _service.GetProductsWithCategory());
         }
 
         [HttpGet]
